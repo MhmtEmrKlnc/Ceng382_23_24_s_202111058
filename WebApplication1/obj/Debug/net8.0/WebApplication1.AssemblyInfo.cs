793313bb899caa669fe2c0fe8d7147138738f652
MhmtEmrKlnc <mehkl007@gmail.com>
2024-03-27 11:30:51 +0300

    kendi degistirdigi Debug'ın icindeki bazı seyler

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10c2a3d69aa0c0b5f805ced85d34ab47b480316")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67ab2e20e2daa297c57597830c8529bc7f8ae1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
