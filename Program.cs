3cd64522908708e96140fc5cc937170d91b3b48c
MhmtEmrKlnc <mehkl007@gmail.com>
2024-03-31 21:32:04 +0300

    Bazu kücük duzeltmeler ve eklemeler yapıldı

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading.Channels;
 using Microsoft.VisualBasic;
 
 public class RoomData
@@ -29,19 +30,28 @@ class Program
 {
     static void addReservation(RoomData roomData, ReservationHandler handler, string id, string name, int day, int hour)
     {
-        Reservation reservation = new Reservation(id, name, day, hour);
+
         bool check = false;
         if (roomData?.Rooms != null)
         {
             foreach (var room in roomData.Rooms)
             {
-                if (reservation.room != null)
+
+                if (id == room.roomId)
                 {
-                    if (reservation.room.roomId == room.roomId)
+                    check=true;
+                    if (name == "" || day < 1 || day > 7 || hour < 0 || hour > 24)
                     {
+                        Console.WriteLine("Gerekli kısımlar(id, name, day, hour) boş bırakılamaz!\nDay 1-7 arası bir deger, hour 00-23 arası bir deger olmalı!\n");
+                    }
+                    else if(room.roomId=="" || room.roomName=="" || room.capacity==""){
+                        Console.WriteLine("Boyle bir oda bulunmamaktadır\n");
+                    }
+                    else
+                    {
+                        Reservation reservation = new Reservation(id, name, day, hour);
                         reservation.room.roomName = room.roomName;
                         reservation.room.capacity = room.capacity;
-                        check = true;
                         handler.addReservation(reservation);
                     }
 
@@ -51,8 +61,17 @@ class Program
 
         if (check == false)
         {
-            Console.WriteLine(id + " id'li bir oda bulunmamaktadır\n");
-            handler.deleteReservation(reservation);
+            if (id == "")
+            {
+                Console.WriteLine("id kısmı boş bırakılamaz, lutfen gecerli id'li bir oda giriniz\n");
+
+            }
+            else
+            {
+                Console.WriteLine(id + " id'li bir oda bulunmamaktadır\n");
+
+            }
+
         }
 
     }
@@ -63,8 +82,15 @@ class Program
         {
             //toDo insde try catch
             //path to json into string
+            //define file path
             string jsonFilePath = "Data.json";
+
+            //Read from json
+            //1->json to text
+            //burası kodu patlatabilen bir yer, path'i bulamazsa patlar
             string jsonString = File.ReadAllText(jsonFilePath);
+
+            //2->decode text into meaningful classes
 
             //options to read
 
@@ -75,18 +101,24 @@ class Program
             };
 
             //read try catch
+            //options yerine new JsonSerializerOptions() yazabiliriz yukarıdakini yazmaya gerek olmadan
             var roomData = JsonSerializer.Deserialize<RoomData>(jsonString, options);
 
             if (roomData?.Rooms != null)
             {
                 foreach (var room in roomData.Rooms)
                 {
-                    Console.WriteLine($"Room Id: {room.roomId}, Name: {room.roomName}, Capacity: {room.capacity}");
+                    if (room.roomId != "" && room.roomName != "" && room.capacity != "")
+                    {
+                        Console.WriteLine($"Room Id: {room.roomId}, Name: {room.roomName}, Capacity: {room.capacity}");
+                    }
+
                 }
             }
             Console.WriteLine("\n");
             ReservationHandler reservationHandler = new ReservationHandler(7, 24);
 
+            //json ici bos gelebilir, bu yuzden null olabilir, bu yuzden ? koyuyoruz. Kodu patlatabilir bos gelirse
             if (roomData?.Rooms != null)
             {
                 addReservation(roomData, reservationHandler, "001", "Mehmet", 5, 6);
@@ -101,13 +133,28 @@ class Program
                 addReservation(roomData, reservationHandler, "001", "Mehmet", 5, 7);
                 addReservation(roomData, reservationHandler, "001", "Mehmet", 5, 7); // aynısından ekleme denemesi
                 addReservation(roomData, reservationHandler, "005", "Mehmet", 4, 11);
+                addReservation(roomData, reservationHandler, "", "", 4, 11); // bos deneme
+                addReservation(roomData, reservationHandler, "003", "", 5, 23); // yanlıs degerler deneme
+                addReservation(roomData, reservationHandler, "003", "Mehmet", -1, 23); // yanlıs degerler deneme
+                addReservation(roomData, reservationHandler, "003", "", 5, 27); // yanlıs degerler deneme
             }
 
             reservationHandler.displayWeeklySchedule();
         }
+
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine($"File not found! - {e.Message}");
+        }
+        catch (NullReferenceException e)
+        {
+            Console.WriteLine($"Null exception! - {e.Message}");
+        }
         catch (Exception e)
         {
-            Console.WriteLine(e.Message);
+            // Diğer hatalar
+            Console.WriteLine($"Exception: {e.Message}");
         }
+
     }
 }
--- ReservationHandler.cs
+++ ReservationHandler.cs
@@ -2,33 +2,41 @@ public class ReservationHandler
 {
     private Reservation[][] reservations;
     private int gldays, glhours;
-    public ReservationHandler(int days, int hours){
-        gldays=days+1;
-        glhours=hours;
-        reservations = new Reservation[days+1][];
-        for(int i=0;i<days+1;i++){
-            reservations[i]=new Reservation[hours];
+    public ReservationHandler(int days, int hours)
+    {
+        gldays = days + 1;
+        glhours = hours;
+        reservations = new Reservation[days + 1][];
+        for (int i = 0; i < days + 1; i++)
+        {
+            reservations[i] = new Reservation[hours];
         }
     }
-    public void addReservation(Reservation reservation){
-        int day = reservation.dateTime.Day;
-        int hour = reservation.time.Hour;
-        if(reservations[day][hour]!=null){
-            Console.WriteLine("Bu gün ve saatte başka bir randevu var, lütfen başka bir gün-saat seçiniz\n");
-        }
-        else{
-            reservations[day][hour] = reservation;
-            Console.WriteLine(reservation.room.roomId+" id'li("+reservation.room.roomName+" isimli) oda "+reservations[day][hour].reserverName+" isimli kişiye "+reservations[day][hour].dateTime.Day+".gün, saat "+reservations[day][hour].time.Hour+":00 için rezerve edilmiştir.\n");
-        }
-        
-    }
-    public void deleteReservation(Reservation reservation){
+    public void addReservation(Reservation reservation)
+    {
         int day = reservation.dateTime.Day;
         int hour = reservation.time.Hour;
 
-        Reservation[][] updatedReservations = new Reservation[gldays+1][];
-        for(int i=0;i<gldays+1;i++){
-            updatedReservations[i]=new Reservation[glhours];
+        if (reservations[day][hour] != null)
+        {
+                    Console.WriteLine("Bu gün ve saatte başka bir randevu var, lütfen başka bir gün-saat seçiniz\n");            
+        }
+        else
+        {
+            reservations[day][hour] = reservation;
+            Console.WriteLine(reservation.room.roomId + " id'li(" + reservation.room.roomName + " isimli) oda " + reservations[day][hour].reserverName + " isimli kişiye " + reservations[day][hour].dateTime.Day + ".gün, saat " + reservations[day][hour].time.Hour + ":00 için rezerve edilmiştir.\n");
+        }
+
+    }
+    public void deleteReservation(Reservation reservation)
+    {
+        int day = reservation.dateTime.Day;
+        int hour = reservation.time.Hour;
+
+        Reservation[][] updatedReservations = new Reservation[gldays + 1][];
+        for (int i = 0; i < gldays + 1; i++)
+        {
+            updatedReservations[i] = new Reservation[glhours];
         }
         for (int i = 0; i < gldays; i++)
         {
@@ -43,13 +51,14 @@ public class ReservationHandler
         }
         reservations = updatedReservations;
 
-        //Console.WriteLine("\nReservation silindi\n");
-        
+        Console.WriteLine("Reservation silindi\n");
+
     }
-    public void displayWeeklySchedule(){
+    public void displayWeeklySchedule()
+    {
 
         Console.WriteLine("\nTablodaki satırlar günleri, sütunlar saatleri göstermektedir. Tablo o gün ve saate denk gelen, rezervasyonu olan odanın id'sini göstermektedir.");
-        Console.Write("  |");
+        Console.Write("   |");
         for (int j = 0; j < glhours; j++)
         {
             Console.Write(j.ToString("D2") + ":00|");
@@ -60,18 +69,18 @@ public class ReservationHandler
             Console.Write(i + " | ");
             for (int j = 0; j < glhours; j++)
             {
-                
-                if(reservations[i][j]==null){
-                    Console.Write("--- | ");
+
+                if (reservations[i][j] == null)
+                {
+                    Console.Write(" --- |");
                 }
-                else{
-                    Console.Write(reservations[i][j].room.roomId+" | ");
+                else
+                {
+                    Console.Write(reservations[i][j].room.roomName + "|");
                 }
-                
+
             }
-        Console.Write("\n");
+            Console.Write("\n");
         }
     }
-
-
 }
