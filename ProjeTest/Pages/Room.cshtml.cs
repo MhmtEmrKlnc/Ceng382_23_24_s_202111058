79887e06da02c2e4f111489fb461e9bd895965c7
MhmtEmrKlnc <mehkl007@gmail.com>
2024-05-08 12:52:58 +0300

    Oda eklenebilen Room ve bu roomların listelendigi RoomList pageleri eklendi

---
+++ ProjeTest/Pages/Room.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjeTest.Pages;
+
+public class RoomModel : PageModel
+{
+   [BindProperty]
+    public Room NewRoom { get; set; } = default!;
+
+    public AppDbContext RoomContext= new();
+    private readonly ILogger<RoomModel> _logger;
+
+    public RoomModel(ILogger<RoomModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+    public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewRoom == null)
+            {
+                return Page();
+            }
+            RoomContext.Add(NewRoom);
+            RoomContext.SaveChanges();
+            return RedirectToAction("Get");
+        }
+
+}
+
---
+++ ProjeTest/Pages/RoomList.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjeTest.Pages;
+
+public class RoomListModel : PageModel
+{
+
+    public AppDbContext RoomListContext= new();
+    public static List<Room> RoomList { get;set; } = default!;
+    private readonly ILogger<RoomListModel> _logger;
+
+    public RoomListModel(ILogger<RoomListModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+{
+          RoomList = RoomListContext.Rooms.ToList<Room>();
+}
+    
+
+}
+
