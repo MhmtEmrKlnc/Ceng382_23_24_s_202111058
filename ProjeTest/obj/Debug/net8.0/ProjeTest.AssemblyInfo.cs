c36666f85b5f23be2bf709d3d172b4f45e3d54f0
MhmtEmrKlnc <mehkl007@gmail.com>
2024-05-13 17:00:58 +0300

    kendi değiştirdigi seyler

--- ProjeTest/obj/Debug/net8.0/ProjeTest.AssemblyInfo.cs
+++ ProjeTest/obj/Debug/net8.0/ProjeTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4957f6ae172977af635a9bf2a95a8b0e97b814fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f662daf9d46c7f23c78772a4e01b7f59151718a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
