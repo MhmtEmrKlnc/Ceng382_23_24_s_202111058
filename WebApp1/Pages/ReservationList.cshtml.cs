648b1e4b6886486f27d2c0856d99bf1f08fbd24d
MhmtEmrKlnc <mehkl007@gmail.com>
2024-05-22 12:20:12 +0300

    Tablo ve tabloda reservasyonların gözükmesi yapıldı

--- WebApp1/Pages/ReservationList.cshtml.cs
+++ WebApp1/Pages/ReservationList.cshtml.cs
@@ -1,4 +1,5 @@
-﻿﻿using Microsoft.AspNetCore.Mvc;
+﻿﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApp1.Models;
 
@@ -9,16 +10,26 @@ public class ReservationListModel : PageModel
 
     public LabWebAppDbContext context= new();
     public static List<TblReservation> ReservationList { get;set; } = default!;
+    public static List<TblRoom> RoomList { get;set; } = default!;
+    public static List<IdentityUser> UserList { get;set; } = default!;
     private readonly ILogger<ReservationListModel> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public ReservationListModel(ILogger<ReservationListModel> logger)
+    public static DateOnly startDate;
+
+    public ReservationListModel(ILogger<ReservationListModel> logger, UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
     public void OnGet()
     {
-          ReservationList = context.TblReservations.ToList<TblReservation>();
+        ReservationList = context.TblReservations.ToList<TblReservation>();
+        RoomList=context.TblRooms.ToList<TblRoom>();
+        UserList=_userManager.Users.ToList();
+        startDate=DateOnly.FromDateTime(DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek));
+        
     }
 }
 
